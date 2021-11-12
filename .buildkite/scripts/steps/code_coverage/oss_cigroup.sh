227bb5422db63f0a8ab0725380b818d4a0bb59e5
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-11-12 18:30:11 +0100

    run tests from main path

--- .buildkite/scripts/steps/code_coverage/oss_cigroup.sh
+++ .buildkite/scripts/steps/code_coverage/oss_cigroup.sh
@@ -12,21 +12,23 @@ export JOB=kibana-oss-ciGroup${CI_GROUP}
 echo "--- OSS CI Group $CI_GROUP"
 echo " -> Running Functional tests with code coverage"
 export NODE_OPTIONS=--max_old_space_size=8192
+export CODE_COVERAGE=1
 
-echo " -> making hard link clones"
-cd ..
-cp -RlP kibana "kibana${CI_GROUP}"
-cd "kibana${CI_GROUP}"
+# echo " -> making hard link clones"
+# cd ..
+# cp -RlP kibana "kibana${CI_GROUP}"
+# cd "kibana${CI_GROUP}"
 
-echo " -> running tests from the clone folder"
+#echo " -> running tests from the clone folder"
 node scripts/functional_tests \
   --include-tag "ciGroup$CI_GROUP" \
   --exclude-tag "skipCoverage" || true
 
 yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.functional_merge.config.js
 
-mkdir -p ../kibana/target/kibana-coverage/functional/merge
-mv target/kibana-coverage/functional/merge/coverage-final.json "../kibana/target/kibana-coverage/functional/merge/oss-${CI_GROUP}-coverage.json" || echo "copying coverage file failed"
+#mkdir -p ../kibana/target/kibana-coverage/functional/merge
+#mv target/kibana-coverage/functional/merge/coverage-final.json "../kibana/target/kibana-coverage/functional/merge/oss-${CI_GROUP}-coverage.json" || echo "copying coverage file failed"
+mv target/kibana-coverage/functional/merge/coverage-final.json "target/kibana-coverage/functional/merge/oss-${CI_GROUP}-coverage.json"
 # echo " -> moving junit output, silently fail in case of no report"
 # mkdir -p ../kibana/target/junit
 # mv target/junit/* ../kibana/target/junit/ || echo "copying junit failed"
