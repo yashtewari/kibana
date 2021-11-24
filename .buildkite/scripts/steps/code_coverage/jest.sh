7d2025b90d86bd715d836977b5012da0b0e7ca53
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-11-24 01:38:19 +0100

    copy main script for parallel jest run

--- .buildkite/scripts/steps/code_coverage/jest.sh
+++ .buildkite/scripts/steps/code_coverage/jest.sh
@@ -8,17 +8,21 @@ is_test_execution_step
 
 .buildkite/scripts/bootstrap.sh
 
-echo '--- Jest with coverage'
-node --max-old-space-size=6144 scripts/jest --ci --maxWorkers=10 --coverage --coverageReporters json || true
+# echo '--- Jest with coverage'
+# node --max-old-space-size=6144 scripts/jest --ci --maxWorkers=10 --coverage --coverageReporters json || true
 
-# change name so report won't be overwritten
-mv target/kibana-coverage/jest/coverage-final.json ./target/kibana-coverage/jest/coverage-jest.json
+# # change name so report won't be overwritten
+# mv target/kibana-coverage/jest/coverage-final.json ./target/kibana-coverage/jest/coverage-jest.json
 
-echo '--- Jest Integration Tests with coverage'
-node --max-old-space-size=5120 scripts/jest_integration  --ci --coverage --coverageReporters json || true
+# echo '--- Jest Integration Tests with coverage'
+# node --max-old-space-size=5120 scripts/jest_integration  --ci --coverage --coverageReporters json || true
 
-echo "--- Combine code coverage in a single report"
-yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.jest.config.js
+# echo "--- Combine code coverage in a single report"
+# yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.jest.config.js
 
-echo "--- Archive combined jest report"
-tar -czf kibana-jest-coverage.tar.gz target/kibana-coverage/jest-combined && rm -rf target/kibana-coverage/jest-combined
+# echo "--- Archive combined jest report"
+# tar -czf kibana-jest-coverage.tar.gz target/kibana-coverage/jest-combined && rm -rf target/kibana-coverage/jest-combined
+
+echo '--- Jest coverage'
+
+.buildkite/scripts/steps/code_coverage/jest_parallel.sh
