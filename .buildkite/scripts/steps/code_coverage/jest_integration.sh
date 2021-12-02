398dcc695bde314b97d3b8efe1d215ddbdb9e690
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-12-02 01:31:14 +0100

    increase heap size for integration tests, update sed command

--- .buildkite/scripts/steps/code_coverage/jest_integration.sh
+++ .buildkite/scripts/steps/code_coverage/jest_integration.sh
@@ -9,4 +9,5 @@ is_test_execution_step
 .buildkite/scripts/bootstrap.sh
 
 echo '--- Jest Integration Tests with coverage'
-node --max-old-space-size=5120 scripts/jest_integration  --ci --coverage --coverageReporters json || true
+node --max-old-space-size=14336 scripts/jest_integration  --ci --coverage --coverageReporters json || true
+mv target/kibana-coverage/jest/coverage-final.json "target/kibana-coverage/jest/jest-integration-coverage.json"
