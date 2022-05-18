1f122313cbee52ce9df588a5bf316cc1d381f978
Tre' Seymour <wayne.seymour@elastic.co>
2022-05-18 13:08:48 +0200

    Use old sed

--- .buildkite/scripts/steps/code_coverage/ingest.sh
+++ .buildkite/scripts/steps/code_coverage/ingest.sh
@@ -32,10 +32,10 @@ echo "--- collect VCS Info"
 
 echo "--- Replace path in coverage json files"
 export COVERAGE_TEMP_DIR=$KIBANA_DIR/target/kibana-coverage
-printf "### ls -R ${COVERAGE_TEMP_DIR}: \n$(ls -R ${COVERAGE_TEMP_DIR})\n"
-#sed -i -e "s|/var/lib/buildkite-agent/builds/kb-n2-4-spot-2-[[:alnum:]\-]\{16\}/elastic/kibana-code-coverage-main/kibana|${KIBANA_DIR}|g" $COVERAGE_TEMP_DIR/**/*.json
-sed -ie "s|\/var\/lib\/buildkite-agent\/builds\/kb-n2-4-spot-2-[[:alnum:]\-]*\/elastic\/kibana-code-coverage-main\/kibana|${KIBANA_DIR}|g" \
+sed -i -e "s|/var/lib/buildkite-agent/builds/kb-n2-4-spot-2-[[:alnum:]\-]\{16\}/elastic/kibana-code-coverage-main/kibana|${KIBANA_DIR}|g" \
   $COVERAGE_TEMP_DIR/**/*.json
+#sed -ie "s|\/var\/lib\/buildkite-agent\/builds\/kb-n2-4-spot-2-[[:alnum:]\-]*\/elastic\/kibana-code-coverage-main\/kibana|${KIBANA_DIR}|g" \
+#  $COVERAGE_TEMP_DIR/**/*.json
 
 echo "--- Jest: merging coverage files and generating the final combined report"
 yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.jest.config.js
